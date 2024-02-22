31f252a5a5013170c931527ea37babb09b20d1ef
John Victor <84483840+Johnviti@users.noreply.github.com>
2024-02-21 23:36:09 -0300

    Feat: Adicionando animações no Formuário

--- app-curso-marketing-digital/src/components/Form/Form.js
+++ app-curso-marketing-digital/src/components/Form/Form.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 import './Form.css';
+import Lottie from 'react-lottie';
+import animationData from './animation/Animation.json';
+import animationDAtaDino from './animation/AnimationDino.json';
 
 import logo from '../../assets/img/logo.png';
 
@@ -13,13 +16,45 @@ const Form = () => {
                 <h1>APRENDA O MARKETING E EVOLUA SUA MENTE</h1>
                 <p>Lorem ipsum dolor sit amet. Nullam ullamco elit sed metus tincidunt, sed commodo velit posuere.</p>
             </div>
+            <div className="lottie">
+                <Lottie 
+                    options={{
+                        loop: true,
+                        autoplay: true,
+                        animationData: animationData,
+                        rendererSettings: {
+                            preserveAspectRatio: 'xMidYMid slice'
+                        }
+                    }}
+                    height={200}
+                    width={200}
+                />
+            </div>
         </div> 
         <div className="conteudo_form">
             <div className="conteudo_principal">
                 <div className="texto_form">
-                    <h1>Mais de 200 alunos</h1>
-                    <h1>Foram empregados</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper elit sed metus tincidunt, sed commodo velit posuere.</p>
+                    <div className="container_texto_animation">
+                        <div className="texto_formulario_container">
+                            <h1>Mais de 200 alunos</h1>
+                            <h1>Foram empregados</h1>
+                        </div> 
+                        <div className="lottieDivo">
+                            <Lottie 
+                                options={{
+                                    loop: true,
+                                    autoplay: true,
+                                    animationData: animationDAtaDino,
+                                    rendererSettings: {
+                                        preserveAspectRatio: 'xMidYMid slice'
+                                    }
+                                }}
+                                height={100}
+                                width={100}
+                            />
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper elit sed metus tincidunt.</p>
                 </div>
                 <div className="grupo_input">
                     <div className="input">
